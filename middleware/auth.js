4cd73bf64889543210a9eec25f35738f68a0b859
jolly01008 <jollgmll292@gmail.com>
2023-09-26 22:15:26 +0800

    feat:add signin authenticated

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+const { ensureAuthenticated } = require('../helpers/auth-helpers')
+
+const authenticated = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+
+module.exports = { authenticated }
