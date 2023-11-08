2353a386441bd0c4015d0894c982cf8133486484
jolly01008 <jollgmll292@gmail.com>
2023-11-09 00:42:01 +0800

    modify tableName spelling error in course model

--- models/course.js
+++ models/course.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Course',
-    tablelName: 'Courses',
+    tableName: 'Courses',
     underscored: true
   })
   return Course
